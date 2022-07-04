15bc3156e02886e31feb79af2e1f492af57e60ac
classmate-xi-xi <yaowenxi123@qq.com>
2022-07-04 17:40:12 +0800

    53

---
+++ src/topic/Topic53.java
@@ -0,0 +1,90 @@
+package topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: ywx
+ * @description 53最大子数组和
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ * 子数组 是数组中的一个连续部分。
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6
+ * @Date: 2022/07/04
+ */
+
+public class Topic53 {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            //sum加下一个数小于0 直接从nums【i】 开始运算 其和值一定
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划 没想到
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray1(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    /**
+     * 分治 更不会。。。。
+     */
+    public class Status {
+        public int lSum, rSum, mSum, iSum;
+
+        public Status(int lSum, int rSum, int mSum, int iSum) {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+
+    public Status getInfo(int[] a, int l, int r) {
+        if (l == r) {
+            return new Status(a[l], a[l], a[l], a[l]);
+        }
+        int m = (l + r) >> 1;
+        Status lSub = getInfo(a, l, m);
+        Status rSub = getInfo(a, m + 1, r);
+        return pushUp(lSub, rSub);
+    }
+
+    public Status pushUp(Status l, Status r) {
+        int iSum = l.iSum + r.iSum;
+        int lSum = Math.max(l.lSum, l.iSum + r.lSum);
+        int rSum = Math.max(r.rSum, r.iSum + l.rSum);
+        int mSum = Math.max(Math.max(l.mSum, r.mSum), l.rSum + r.lSum);
+        return new Status(lSum, rSum, mSum, iSum);
+    }
+
+    public int maxSubArray3(int[] nums) {
+        return getInfo(nums, 0, nums.length - 1).mSum;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(new Topic53().maxSubArray(nums));
+    }
+}
