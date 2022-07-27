7b8592478406d42f092e7e8041456fee36bbb75c
classmate-xi-xi <yaowenxi123@qq.com>
2022-07-27 21:17:35 +0800

    全排列

---
+++ src/topic/Topic46.java
@@ -0,0 +1,49 @@
+package topic;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: ywx
+ * @description 全排列
+ * @Date: 2022/07/27
+ */
+
+public class Topic46 {
+    List<List<Integer>> res = new LinkedList<>();
+
+
+    public List<List<Integer>> permute(int[] nums) {
+        // 路径：记录在 track 中
+        LinkedList<Integer> track = new LinkedList<>();
+        // 「路径」中的元素会被标记为 true，避免重复使用
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, track, used);
+        return res;
+    }
+
+    // 选择列表：nums 中不存在于 track 的那些元素（used[i] 为 false）
+    // 结束条件：nums 中的元素全都在 track 中出现
+    private void backtrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        // 触发结束条件
+        if (track.size() == nums.length) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            // 排除不合法的选择
+            if (used[i]) {
+                // nums[i] 已经在 track 中，跳过
+                continue;
+            }
+            // 做选择
+            track.add(nums[i]);
+            used[i] = true;
+            // 进入下一层决策树
+            backtrack(nums, track, used);
+            // 取消选择
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
