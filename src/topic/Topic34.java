58253781b833593de47ce079b61aea0df83088b8
classmate-xi-xi <yaowenxi123@qq.com>
2022-07-30 19:13:26 +0800

    二分查找 左右边界问题

---
+++ src/topic/Topic34.java
@@ -0,0 +1,47 @@
+package topic;
+
+/**
+ * @author: ywx
+ * @description 在排序数组中查找元素的第一个和最后一个位置
+ * @Date: 2022/07/30
+ */
+
+public class Topic34 {
+    public int[] searchRange(int[] nums, int target) {
+        int left =leftBound(nums, target);int right = rightBound(nums, target);
+        return new int[]{left, right};
+    }
+
+    private int leftBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
+    }
+
+    private int rightBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1;
+        }
+        return right;
+    }
+
+}
