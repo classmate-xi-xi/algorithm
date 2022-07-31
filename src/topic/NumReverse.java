43d7e6da770f50512af4c37981339e747248990e
classmate-xi-xi <yaowenxi123@qq.com>
2022-07-31 20:51:56 +0800

    整数中每位上的数字进行反转

--- src/topic/NumReverse.java
+++ src/topic/NumReverse.java
@@ -1,4 +1,4 @@
-package easy;
+package topic;
 
 /**
  * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
@@ -19,10 +19,12 @@ public class NumReverse {
         int ans = 0;
         while (x != 0) {
             int pop = x % 10;
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7))
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) {
                 return 0;
-            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8))
+            }
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)){
                 return 0;
+            }
             ans = ans * 10 + pop;
             x /= 10;
         }
