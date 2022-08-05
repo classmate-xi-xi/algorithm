84ed61b46e2c701250a8f4dab5dd68d79abe1a0a
classmate-xi-xi <yaowenxi123@qq.com>
2022-08-05 19:30:54 +0800

    623 在二叉树中增加一行

---
+++ src/topic/Topic623.java
@@ -0,0 +1,46 @@
+package topic;
+
+/**
+ * @author: ywx
+ * @description 在二叉树中增加一行
+ * @Date: 2022/08/05
+ */
+
+public class Topic623 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+
+}
