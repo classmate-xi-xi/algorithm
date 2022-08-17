5970a8a7c7c4c93463f0c4a7cc2452aa9b9da7b4
classmate-xi-xi <yaowenxi123@qq.com>
2022-08-17 17:21:07 +0800

    1302 层数最深叶子节点的和 BFS

---
+++ src/topic/Topic1302.java
@@ -0,0 +1,55 @@
+package topic;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: ywx
+ * @description 层数最深叶子节点的和
+ * @Date: 2022/08/17
+ */
+
+public class Topic1302 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return sum;
+    }
+}
