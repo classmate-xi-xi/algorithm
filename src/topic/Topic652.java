57231e38f3807ba9614311a12a6c2f2d1c9e7fd8
classmate-xi-xi <yaowenxi123@qq.com>
2022-09-05 19:23:26 +0800

    652 寻找重复的子树

---
+++ src/topic/Topic652.java
@@ -0,0 +1,80 @@
+package topic;
+
+import java.util.*;
+
+/**
+ * @author: ywx
+ * @description 寻找重复的子树
+ * @Date: 2022/09/05
+ */
+
+public class Topic652 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return super.equals(o);
+        }
+    }
+
+    /**
+     * 官方解
+     */
+    Map<String, TreeNode> seen = new HashMap<String, TreeNode>();
+    Set<TreeNode> repeat = new HashSet<TreeNode>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        dfs(root);
+        return new ArrayList<TreeNode>(repeat);
+    }
+
+    public String dfs(TreeNode node) {
+        if (node == null) {
+            return "";
+        }
+        /**
+         * String serial = node.val +
+         *                 "(" +
+         *                 dfs(node.left) +
+         *                 ")(" +
+         *                 dfs(node.right) +
+         *                 ")";
+         */
+        StringBuilder sb = new StringBuilder();
+        sb.append(node.val);
+        sb.append("(");
+        sb.append(dfs(node.left));
+        sb.append(")(");
+        sb.append(dfs(node.right));
+        sb.append(")");
+        String serial = sb.toString();
+
+        if (seen.containsKey(serial)) {
+            repeat.add(seen.get(serial));
+        } else {
+            seen.put(serial, node);
+        }
+        return serial;
+    }
+}
