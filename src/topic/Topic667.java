d611f3d66d8c22fa5a82c0d11b203eed470d2a9e
classmate-xi-xi <yaowenxi123@qq.com>
2022-09-08 19:10:51 +0800

    667 优美的排列 II

---
+++ src/topic/Topic667.java
@@ -0,0 +1,34 @@
+package topic;
+
+/**
+ * @author: ywx
+ * @description 优美的排列 II
+ * 给你两个整数 n 和 k ，请你构造一个答案列表 answer ，该列表应当包含从 1 到 n 的 n 个不同正整数，并同时满足下述条件：
+ * 假设该列表是 answer=[a1, a2, a3, ... , an]
+ * 那么列表 [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] 中应该有且仅有 k 个不同整数
+ * 返回列表 answer 。如果存在多种答案，只需返回其中 任意一种
+ * 输入：n = 3, k = 1
+ * 输出：[1, 2, 3]
+ * 解释：[1, 2, 3] 包含 3 个范围在 1-3 的不同整数，并且 [1, 1] 中有且仅有 1 个不同整数：1
+ * @Date: 2022/09/08
+ */
+
+public class Topic667 {
+    public int[] constructArray(int n, int k) {
+        int[] answer = new int[n];
+        int idx = 0;
+        for (int i = 1; i < n - k; ++i) {
+            answer[idx] = i;
+            ++idx;
+        }
+        for (int i = n - k, j = n; i <= j; ++i, --j) {
+            answer[idx] = i;
+            ++idx;
+            if (i != j) {
+                answer[idx] = j;
+                ++idx;
+            }
+        }
+        return answer;
+    }
+}
