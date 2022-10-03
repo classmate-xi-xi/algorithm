0e92a1e4aa613a98d913a2b5cd82774785bd672f
classmate-xi-xi <yaowenxi123@qq.com>
2022-10-03 21:06:38 +0800

    1784. 检查二进制字符串字段

---
+++ src/topic/Topic1784.java
@@ -0,0 +1,13 @@
+package topic;
+
+/**
+ * @author: ywx
+ * @description 检查二进制字符串字段
+ * @Date: 2022/10/03
+ */
+
+public class Topic1784 {
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
