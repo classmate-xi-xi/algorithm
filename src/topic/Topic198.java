095c7db29b554c596801812e5012d74c1c8c2895
ywx <yaowenxi123@qq.com>
2023-09-20 00:59:02 +0800

    打家劫舍

---
+++ src/topic/Topic198.java
@@ -0,0 +1,62 @@
+package topic;
+
+/**
+ * @author: ywx
+ * @description: 打家劫舍
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。
+ * 每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你不触动警报装置的情况下，一夜之内能够偷窃到的最高金额。
+ * @date: 2023/9/19
+ */
+public class Topic198 {
+    /**
+     * 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     * 偷窃到的最高金额 = 1 + 3 = 4 。
+     *
+     * @param nums [1,2,3,1]
+     * @return 4
+     */
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i < nums.length + 1; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+        }
+        return dp[nums.length];
+    }
+
+    public int rob2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        // 计算两边边界,取最大金额数
+        return Math.max(robRange2(nums, 0, nums.length - 2), robRange2(nums, 1, nums.length - 2));
+    }
+
+    public int robRange2(int[] nums, int start, int end) {
+        // 即dp[0]
+        int first = nums[start];
+        // 即dp[1]
+        int second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(second, first + nums[i]);
+            first = temp;
+        }
+        return second;
+    }
+}
--- src/topic/Topic213.java
+++ src/topic/Topic213.java
@@ -22,14 +22,20 @@ public class Topic213 {
         } else if (length == 2) {
             return Math.max(nums[0], nums[1]);
         }
+        // 不能同时偷相邻的房屋,由于收尾相邻所以有两种边界
+        // 0, nums.length - 2 和 1, nums.length - 1
+        // 所以需计算两种边界的最大值
         return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
     }
 
     public int robRange(int[] nums, int start, int end) {
+        // 可所窃取的金额总数
         int first = nums[start];
+        // 取可最大窃取金额数
         int second = Math.max(nums[start], nums[start + 1]);
         for (int i = start + 2; i <= end; i++) {
             int temp = second;
+            // 寻找子问题的最优解
             second = Math.max(first + nums[i], second);
             first = temp;
         }
